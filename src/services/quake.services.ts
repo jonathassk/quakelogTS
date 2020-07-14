6fc64faa32ed9c3f22a0e4f3fe38eb760e47f3c4
jonathas fonseca <jonathas09@gmail.com>
2020-07-13 22:06:51 -0300

    removendo documento desnecessario

--- src/services/quake.services.ts
+++ src/services/quake.services.ts
@@ -3,7 +3,7 @@ import * as path from "path";
 
 export class CreateLogs {
   static matchLog (req, res): any{
-    fs.readFile(path.join(__dirname, './games.log'), 'utf8', (err, data) => {
+    fs.readFile(path.join(__dirname, '../data/games.log'), 'utf8', (err, data) => {
       if (err) {
         console.log(err)
       }
