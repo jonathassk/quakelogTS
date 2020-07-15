f1a41a9fbf5a67489c3d062691186df1aeeaf861
jonathas fonseca <jonathas09@gmail.com>
2020-07-15 19:04:16 -0300

    regex e funções de init game/ user change e kills count funcionando corretamente

--- src/helpers/transform-log-to-array.ts
+++ src/helpers/transform-log-to-array.ts
@@ -5,7 +5,6 @@ class Game {
 }
 
 
-
 export class TransformLogToArray {
   static createObjectGame(data: Array<string>) {
     let game = new Game()
@@ -14,23 +13,29 @@ export class TransformLogToArray {
         game.total_kills = 0
         game.players = []
         game.kills = {}
-      }
-      if (action.match(/Kill: ([0-9]+) ([0-9]+)/)){
-        //console.log(action.match(/Kill: ([0-9]+) ([0-9]+) ([0-9]+): ([\w.]+)/))
+        console.log('init', game)
       }
       if (action.match(/ClientUserinfoChanged: ([0-9]+) n\\(.*)\\t\\([0-9]+)/)) {
-        console.log(action.match(/ClientUserinfoChanged: ([0-9]+) n\\(.*)\\t\\([0-9]+)/))
+        let userName = action.match(/ClientUserinfoChanged: ([0-9]+) n\\(.*)\\t\\([0-9]+)/)[2]
+        let userId: number = parseInt(action.match(/ClientUserinfoChanged: ([0-9]+) n\\(.*)\\t\\([0-9]+)/)[1])
+        if (!game.players[userId - 2]) {
+          game.players.push(userName)
+        } else {
+          game.players[userId - 2] = userName
+        }
+      console.log('change', game)
+      }
+      if (action.match(/Kill: ([0-9]+) ([0-9]+)/)){
+        game.total_kills++
+        //console.log(action.match(/Kill: ([0-9]+) ([0-9]+) ([0-9]+): ([\w.]+)/))
       }
       if (action.match(/ShutdownGame:/)) {
-
+        console.log('end', game)
       }
     })
-
-
-    game.total_kills = 2
-    game.players = ['player1', 'player2']
-    game.kills = {
-      'player 1': 2
-    }
   }
 }
+
+
+//Object.assign
+//array.push
